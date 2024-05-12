3462d4b9081cdd299a8a3233e0d0c3ebcf105045
slepetit <slepetit@student.42angouleme.fr>
2024-05-12 16:33:00 -0400

    maria

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-mysqld_safe
-mysql_secure_installation
+#/etc/init.d/mariadb start OR service mariadb start ==> DONT WORK
+chown -R mysql:mysql /var/lib/mysql
+chmod -R 755 /var/lib/mysql/
+
+if [ ! -d /run/mysqld ]; then 
+    mkdir -p /run/mysqld
+fi
+
+chown -R mysql:mysql /run/mysqld
+chmod -R 755 /run/mysqld/
+
+service mariadb start
+sleep 5
+exec mysqld_safe
+# mysql_secure_installation
 
 # mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 # mysql -e "$SQL_ROOT_PASSWORD"
@@ -9,6 +22,3 @@ mysql_secure_installation
 # mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # mysql -e "FLUSH PRIVILEGES;"
-
-# socket = /run/mysqld/mysqld.sock in conf
-# service mariadb start/status
