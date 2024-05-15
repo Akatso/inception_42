cdc96e71def89cfd63c2b5451dca08c821adf205
slepetit <slepetit@student.42angouleme.fr>
2024-05-15 10:38:14 -0400

    vkjuioqjea

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 /etc/init.d/mariadb start
+# service mariadb start
 
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASS}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASS}';"
-mysql -e -u root "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASS}';"
+mysql -e -u root "ALTER USER 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASS}';"
 mysql -e "FLUSH PRIVILEGES;"
 sleep 5
 service mariadb stop
