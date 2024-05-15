7d6855434858b63278b4d693a4a17f7b6e222f7c
slepetit <slepetit@student.42angouleme.fr>
2024-05-15 10:24:48 -0400

    maria log socket, check localhost in script

---
+++ srcs/requirements/mariadb/conf/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/etc/init.d/mariadb start
+
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASS}';"
+mysql -e -u root "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASS}';"
+mysql -e "FLUSH PRIVILEGES;"
+sleep 5
+service mariadb stop
+exec mysqld_safe
