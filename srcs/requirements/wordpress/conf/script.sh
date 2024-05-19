370735ffbdeed90190043d67e94428e383497c1b
slepetit <slepetit@student.42angouleme.fr>
2024-05-19 12:14:21 -0400

    oh my

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,10 +1,8 @@
-#!bin/bash
+!bin/bash
 
 sleep 10
 
-# ????????????????????????
-wp config create --allow-root
-wp config create --dbname=$SQL_DATABASE
-wp config create --dbuser=$SQL_USER
-wp config create --dbpass=$SQL_PASSWORD 
-wp config create --dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp config create --allow-root --dbname=$SQL_DATABASE
+wp config create --allow-root --dbuser=$SQL_USER
+wp config create --allow-root --dbpass=$SQL_PASSWORD 
+wp config create --allow-root --dbhost=mariadb:3306 --path='/var/www/wordpress'
