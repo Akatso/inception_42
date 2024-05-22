b58daaf55a70037b43427362d3ea1246fcc0b174
slepetit <slepetit@student.42angouleme.fr>
2024-05-22 13:21:38 -0400

    ouais

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -3,9 +3,8 @@
 sleep 10
 
 wp core download --allow-root
-wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=root --dbpass=$SQL_ROOT_PASS --dbhost=mariadb:3306 --skip-check
+wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASS --dbhost=mariadb:3306 --skip-check
 wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
 wp user create --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASS
-echo "???"
 
 exec /usr/sbin/php-fpm7.4 -F
